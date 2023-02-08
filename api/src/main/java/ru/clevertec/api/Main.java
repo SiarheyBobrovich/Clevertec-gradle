752a7be13abf56c506f71ac208a6089625cfe936
Sergey Bobrovich <siarhey1987@gmail.com>
2023-02-08 17:48:50 +0300

    feat: add /api

---
+++ api/src/main/java/ru/clevertec/api/Main.java
@@ -0,0 +1,9 @@
+package ru.clevertec.api;
+
+import ru.clevertec.core.Utils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers("12", "19"));
+    }
+}
