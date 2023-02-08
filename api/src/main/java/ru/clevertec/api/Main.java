cebe006540f25cf24cd7dacf99f933b1a7015e4b
Sergey Bobrovich <siarhey1987@gmail.com>
2023-02-08 19:51:38 +0300

    refactor: api:Main.java

--- api/src/main/java/ru/clevertec/api/Main.java
+++ api/src/main/java/ru/clevertec/api/Main.java
@@ -4,6 +4,15 @@ import ru.clevertec.core.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Utils.isAllPositiveNumbers("12", "19"));
+        if(args.length == 0) {
+            System.out.printf(
+                    "Are 12 & 19 positive numbers?%nAnswer: %s%n", Utils.isAllPositiveNumbers("12", "19"));
+        }else {
+            System.out.printf(
+                    "Are %s positive numbers?%nAnswer: %s%n",
+                    String.join(" & ", args),
+                    Utils.isAllPositiveNumbers(args)
+            );
+        }
     }
 }
